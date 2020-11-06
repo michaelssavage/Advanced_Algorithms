55a62b8160349ad07d802c315e6d6ba023a4dd53
Michael S <33690140+michaelssavage@users.noreply.github.com>
2020-11-06 10:57:13 +0000

    Rename week9/numpyArray.py to Neural_Networks/numpyArray.py

---
+++ Neural_Networks/numpyArray.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def main():
+    print(np.array([[0.86,0.73],[0.82,0.71],[0.82,0.38]]))
--- week9/numpyArray.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def main():
-    print(np.array([[0.86,0.73],[0.82,0.71],[0.82,0.38]]))
